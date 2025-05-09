86f8d1a6de347ecd81fdc459371608f49501bed8
Juyong son <developerjuyong0129@gmail.com>
2025-05-10 08:16:55 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a[100], b[100];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d", &a[i], &b[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("Case #%d: %d\n", i + 1, a[i] + b[i]);
+    }
+
+    return 0;
+}
